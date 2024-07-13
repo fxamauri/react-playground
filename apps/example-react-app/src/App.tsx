7e9e691a0314b1b7308875db6216761a08f1f828
Amauri Dias <athlon.amauri@gmail.com>
2024-07-13 11:59:12 -0300

    feat: project example react app

---
+++ apps/example-react-app/src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>example-react-app</h1>;
+}
+
+export default App;
--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,5 +1,3 @@
-const { rules, plugins } = require("../../apps/web/.eslintrc.cjs");
-
 module.exports = {
     root: true,
     env: { browser: true, es2020: true },
